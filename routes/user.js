b881329ddfc9bbecb17ba4cea438b6ebac15b7b7
Jayakrishnan VR <jayakrishnanvrjk@gmail.com>
2021-02-04 23:02:38 +0530

    part 5 partially completed

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,43 @@
 var express = require('express');
 var router = express.Router();
 var productHelpers = require("../helpers/product-helpers");
+const userHelpers = require('../helpers/user-helpers')
+// const { route } = require('./admin');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   
   productHelpers.getAllProducts().then((products)=>{
     
-    res.render('user/view-products',{admin:false, products});
+    res.render('user/view-products',{products});
   })
 });
+
+router.get('/login',(req,res)=>{
+  res.render('user/login')
+})
+
+router.get('/signup',(req,res)=>{
+  res.render('user/signup')
+})
+
+router.post('/signup',(req,res)=>{
+  userHelpers.doSignup(req.body).then((data)=>{
+     console.log(data)
+  })
+})
+
+router.post('/login', (req,res)=>{
+  userHelpers.doLogin(req.body).then((response)=>{
+    if(response.status){
+      res.redirect('/')
+    }
+    else{
+      res.redirect('/login')
+    }
+  })
+})
+
 module.exports = router;
+
