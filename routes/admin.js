ec1a1a99495925e1d8fdbbd035832985054aec33
Jayakrishnan VR <jayakrishnanvrjk@gmail.com>
2021-05-23 15:11:04 +0530

    .gitignore added

--- routes/admin.js
+++ routes/admin.js
@@ -1,78 +1,67 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 var productHelpers = require("../helpers/product-helpers");
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
+router.get("/", function (req, res, next) {
   productHelpers.getAllProducts().then((products) => {
-
-    res.render('admin/view-products', { admin: true, products });
-
-  })
+    res.render("admin/view-products", { admin: true, products });
+  });
 });
 
-router.get('/logine', (req, res) => {
+router.get("/logine", (req, res) => {
   if (req.session.user) {
-    res.redirect('/')
-  }
-  else {
-    res.render('admin/logine', { "loginErr": req.session.userLoginErr })
+    res.redirect("/");
+  } else {
+    res.render("admin/logine", { loginErr: req.session.userLoginErr });
     req.session.userLoginErr = false;
   }
+});
 
-})
-
-router.post('/logine', (req, res) => {
-
+router.post("/logine", (req, res) => {
   console.log(req.body);
-  res.render('admin/logine', { "loginErr": req.session.adminLoginErr })
+  res.render("admin/logine", { loginErr: req.session.adminLoginErr });
   req.session.userLoginErr = false;
+});
 
-})
+router.get("/add-product", function (req, res) {
+  res.render("admin/add-product");
+});
 
-
-
-router.get('/add-product', function (req, res) {
-  res.render('admin/add-product')
-})
-
-router.post('/add-product', (req, res) => {
-
+router.post("/add-product", (req, res) => {
   productHelpers.addProduct(req.body, (id) => {
-    let image = req.files.Image
+    let image = req.files.Image;
     image.mv("./public/product-images/" + id + ".jpg", (err) => {
       if (!err) {
         res.render("admin/add-product");
-      }
-      else {
+      } else {
         console.log(err);
       }
-    })
-  })
-})
+    });
+  });
+});
 
-router.get('/delete-product/:id', (req, res) => {
+router.get("/delete-product/:id", (req, res) => {
   let proID = req.params.id;
   productHelpers.deleteProduct(proID).then((response) => {
-    res.redirect('/admin')
-  })
-})
+    res.redirect("/admin");
+  });
+});
 
-router.get('/edit-product/:id', async (req, res) => {
+router.get("/edit-product/:id", async (req, res) => {
   let product = await productHelpers.getProductDetails(req.params.id);
-  //console.log(product);
-  res.render('admin/edit-product', { product });
-})
+  res.render("admin/edit-product", { product });
+});
 
-router.post('/edit-product/:id', (req, res) => {
+router.post("/edit-product/:id", (req, res) => {
   productHelpers.updateProduct(req.params.id, req.body).then(() => {
-    let id = req.params.id
-    res.redirect('/admin')
+    let id = req.params.id;
+    res.redirect("/admin");
     if (req.files.Image) {
       let image = req.files.Image;
-      image.mv("./public/product-images/" + id + ".jpg")
+      image.mv("./public/product-images/" + id + ".jpg");
     }
-  })
-})
+  });
+});
 
 module.exports = router;
